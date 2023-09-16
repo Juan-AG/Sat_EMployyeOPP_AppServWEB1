bae03da2420442287c2c427da0a93158c77433bf
Juan David Alvarez Garcia <ldsjuan8@gmail.com>
2023-09-16 18:02:31 -0500

    HourlyEmployee

---
+++ EmployeeOOP/EmployeeOOP/Clases/HourlyEmployee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeOOP.Clases
+{
+    public class HourlyEmployee:Employee
+    {
+        #region Propiedades
+        public float? Hours { get; set; }
+        public float? HourValue { get; set; }
+        #endregion
+
+        #region Metodos
+        public HourlyEmployee()
+        {
+                
+        }
+
+        public override decimal GetValueToPay()
+        {
+            return Convert.ToDecimal(Hours * HourValue);
+        }
+
+        public override string ToString()
+        {
+            return $" {base.ToString()} "+
+                $"Salario por horas:{GetValueToPay()}";
+        }
+        #endregion
+    }
+}
